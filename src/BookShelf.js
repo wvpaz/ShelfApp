bbcb8535967c47edc5841a842ab18f331faa885c
Wendel V. da Paz <wendelpaz@gmail.com>
2018-07-12 15:21:34 -0300

    Commit inicial

---
+++ src/BookShelf.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Book from "./Book"
+
+function BookShelf(props) {
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">Currently Reading</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    <li>
+                        <Book title="To Kill a Mockingbird" author="Harper Lee" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }} />
+                    </li>
+                    <li>
+                        <Book title="Ender's Game" author="Orson Scott Card" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }} />
+                    </li>
+                </ol>
+            </div>
+        </div>
+    );
+}
+
+export default BookShelf;
